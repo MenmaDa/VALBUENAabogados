4cde45aa0a86b9585149fdbe91e9422f384ded60
Daniel Agudelo <dalfonsoagudelo@ucompensar.edu.co>
2024-09-12 22:24:27 -0500

    Aplicacion Valbuena Abogados

---
+++ AppAbogados/app/src/main/java/com/example/appabogados/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.appabogados;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.appabogados.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
